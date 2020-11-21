5d0986163decec98dc90f8f779b1579de331869f
Hongda Zheng <derrickzheng00@gmail.com>
2020-11-21 08:29:04 -0500

    first commit

---
+++ Assets/Scripts/LoadReview.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadReview : MonoBehaviour
+{
+    public void LoadReviewScene() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Assets/View_models.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class View_models : MonoBehaviour
+{
+    public GameObject menu;
+    public GameObject survey;
+
+    public void Toggle_Changed(bool newValue) {
+        menu.SetActive(newValue);
+    }
+
+}
